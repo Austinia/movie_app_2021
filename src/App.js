b78710079c58537230a9d6911c251780116443b8
Austin <kimdy90919@gmail.com>
2021-03-28 01:31:44 +0900

    first commit 2021/03/28/0131

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({name,picture}){
+  return(
+    <div>
+      <h3>I like {name}!!!!</h3>
+      <img src={picture} alt={name}/>
+    </div>
+    
+  )
+}
+
+// id를 통해 react가 컴포넌트가 각각 다르다는걸 알게 함.
+const foodILIKE = [{id:1,name:"kimchi",image:"https://www.maangchi.com/wp-content/uploads/2015/07/tongbaechu-kimchi.jpg"},
+{id:2,name:"pizza",image:"https://i.ytimg.com/vi/P-Ojv_1veCg/maxresdefault.jpg"},
+{id:3,name:"chicken",image:"https://cdn0.wideopeneats.com/wp-content/uploads/2016/10/FriedChicken-950x535.jpeg"}];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi there!!</h1>
+      {foodILIKE.map(dish => (
+      <Food key={dish.id} name={dish.name} picture={dish.image}/>))}
     </div>
   );
 }
