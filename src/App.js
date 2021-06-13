cff3e973a11c1ae3d352daaa9cbb6328d3d168dc
Austinia <kimdy90919@gmail.com>
2021-06-13 20:23:48 +0900

    2021/06/13/2023

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./app.css";
 /*
 class App extends React.Component{
   //state 정의
@@ -35,7 +36,7 @@ class App extends React.Component {
   getMovies = async () => { //async 비동기 함수
     const { data: { data: { movies }}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
     this.setState({ movies, isLoading: false });
-  }; //await 뭘 기다리는지, movies.data.data.movies 보다 좋은 방법
+  }; // movies = movies:movies, await 뭘 기다리는지, movies.data.data.movies 보다 좋은 방법
   componentDidMount(){ // 첫 렌더 완료시 실행되는 함수
     // setTimeout(() => { //6초 뒤에 스테이트 변경
     //   this.setState({isLoading: false});
@@ -43,9 +44,28 @@ class App extends React.Component {
     this.getMovies();
   };
   render(){
-    const { isLoading, movies } = this.state;
-    return <div>{isLoading ? "Loading..." : movies.map(movie => (<Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} /> // map은 언제나 return해야 한다.
-    ))}</div>;
+    const { isLoading, movies } = this.state; // 일일히 this.state를 적지 않게 해준다.
+    return <section class="container">
+      {isLoading ? (
+      <div class="loader">
+        <span class="loader__text_">Loading...</span>
+      </div>
+    ) : (
+      <div class="movies">
+        {movies.map(movie => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+            genres={movie.genres}
+          /> // map은 언제나 return해야 한다.
+        ))}
+      </div>
+    )}
+    </section>;
   };
 };
 
